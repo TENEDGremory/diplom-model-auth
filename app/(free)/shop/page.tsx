771857302a2cdf1520d7be16b981b7b9e3828631
TENEDGremory <130216610+TENEDGremory@users.noreply.github.com>
2024-05-16 20:53:28 +0300

    123

--- app/(free)/shop/page.tsx
+++ app/(free)/shop/page.tsx
@@ -5,7 +5,7 @@ const ShopPage = async () =>{
   return(
     <div className="relative top-16">
       {card.map((card) =>(
-        <img src={card.imageId} width={50} height={50}/>
+        <img key={card.id} src={card.imageId} width={50} height={50}/>
       ))}
     </div>
   )
