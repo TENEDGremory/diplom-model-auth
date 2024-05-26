d03fe2008d04fd7a0395698f39329cb0b4dedeb5
TENEDGremory <130216610+TENEDGremory@users.noreply.github.com>
2024-05-26 10:43:29 +0300

    123

--- components/shop/shop-store.tsx
+++ components/shop/shop-store.tsx
@@ -33,19 +33,3 @@ const ShopStore = ({ storeCard }: { storeCard: Array<any> }) => {
 
 
 export default ShopStore;
-
-
-
-// <div key={card.id} className="group/card relative w-[300px] h-[400px] bg-white mx-2 my-2 p-4 rounded-md items-center shadow-md">
-//   <div className="w-[260px] h-[260px] overflow-hidden border-2 border-sky-200 rounded-lg bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-cyan-600 to-indigo-900 mx-auto shadow-lg">
-//     <Image priority src={card.imageId} width={260} height={260} alt={card.name} className="transition duration-300 group-hover/card:scale-105" />
-//   </div>
-//   <table className="w-full">
-//     <tbody>
-//       <tr className="text-2xl flex items-center justify-between" >
-//         <th>{card.name} </th>
-//         <th>{card.price} $</th>
-//       </tr>
-//     </tbody>
-//   </table>
-// </div>
