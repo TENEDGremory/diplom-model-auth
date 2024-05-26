04217e7fa38a11bb025899abc675efc63133a70f
TENEDGremory <130216610+TENEDGremory@users.noreply.github.com>
2024-05-26 16:55:28 +0300

    123

--- components/shop/shop-store.tsx
+++ components/shop/shop-store.tsx
@@ -13,9 +13,9 @@ const ShopStore = ({ storeCard }: { storeCard: Array<any> }) => {
   return (
     <div className="relative grid grid-cols-4 z-10 mx-auto">
       {storeCard.map((card) => (
-        <Card key={card.id} className="w-[300px] m-2 p-4 shadow-md space-y-4">
-          <CardHeader className="w-[260px] h-[260px] overflow-hidden border-2 border-sky-200 rounded-lg bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-cyan-600 to-indigo-900 mx-auto shadow-lg">
-            <Image priority src={card.imageId} width={260} height={260} alt={card.name} className="transition duration-300 group-hover/card:scale-105" />
+        <Card key={card.id} className="group/card w-[300px] m-2 p-4 shadow-md space-y-4">
+          <CardHeader className="w-[260px] overflow-hidden border-2 border-sky-200 rounded-lg bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-cyan-600 to-indigo-900 mx-auto shadow-lg">
+            <Image priority src={card.imageId} width={260} height={260} alt={card.name} className="w-[260px] h-[260px] object-cover rounded-sm transition duration-300 group-hover/card:scale-105" />
           </CardHeader>
           <CardContent className="p-0">
             {card.name} {card.price} $
